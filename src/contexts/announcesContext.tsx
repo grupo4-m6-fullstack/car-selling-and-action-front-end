291d7124b9c2928ea90dd3e2de879a698a63b552
joao francisco <joaofrancisco.guarda@gmail.com>
2022-10-28 14:00:21 -0300

    feat: Context for announces, functions need to be created to better interections, base functionality working

--- src/contexts/announcesContext.tsx
+++ src/contexts/announcesContext.tsx
@@ -1,3 +1,13 @@
+import {
+  createContext,
+  useContext,
+  useState,
+} from "react";
+
+interface AuxProps {
+  children: JSX.Element;
+}
+
 interface Announce {
   id: string;
   title: string;
@@ -6,11 +16,11 @@ interface Announce {
   price: string;
   bid: string;
   description: string;
-  cover_img: string;
+  coverImg: string;
   type: string;
 
   // ownerId ou userId??
-  ownerId: string;
+  userId: string;
 
   cloaseAt: string;
   isActive: boolean;
@@ -18,11 +28,56 @@ interface Announce {
   updatedAt: string;
 }
 
+interface AnnounceContextData {
+  announces: Announce[];
+}
+
+interface AnnounceState {
+  announces: Announce[];
+}
+
+const announcesContext =
+  createContext<AnnounceContextData>(
+    {} as AnnounceContextData
+  );
+
 // Not finished yet
-const announcesContext = () => {
+export const AnnouncesProvider = ({
+  children,
+}: AuxProps) => {
+  const [data, setData] = useState<AnnounceState>(
+    () => {
+      const announces = localStorage.getItem(
+        "@teste:announces"
+      );
+
+      if (announces) {
+        return {
+          announces: JSON.parse(announces),
+        };
+      }
+      return {} as AnnounceState;
+    }
+  );
+
   return (
-    <div>announcesContext</div>
+    <announcesContext.Provider
+      value={{
+        announces: data.announces,
+      }}>
+      {children}
+    </announcesContext.Provider>
   );
 };
 
-export default announcesContext;
+export const useAnnounce = () => {
+  const context = useContext(announcesContext);
+
+  if (!context) {
+    throw new Error(
+      "useAnnounce must be used within an AnnounceProvider"
+    );
+  }
+
+  return context;
+};
